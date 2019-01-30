34642bc537516ce06d62fb33443cf78df38e560e
Neil Armitage <flusaka@gmail.com>
2019-01-30 23:54:53 +0000

    Adding react-router-dom and incredibly basic home page. To be expanded on.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home/Home';
+
 import './App.css';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App" />
+      <BrowserRouter>
+        <Route path='/' component={ Home } />
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Home/Home.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Fragment, PureComponent } from 'react';
+
+class Home extends PureComponent {
+    public render() {
+        return (
+            <Fragment>
+                <div>Home</div>
+            </Fragment>
+        );
+    }
+}
+
+export default Home;
